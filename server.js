307f4b1e71cf6a7d9cd64e8164640d28208e72d1
Matt Sadauckas <matthewsadauckas@gmail.com>
2017-12-02 19:40:03 -0700

    WIP Build connections between server and client

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
-const app = express()
 const Rx = require('rxjs/Rx')
+
+const app = express()
 
 app.set('port', process.env.PORT || 3001)
 
@@ -9,6 +10,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.get('/api', (req, res) => {
+  //TODO: This is where the database goes
   res.json({stuff: 'poop'})
 })
 
