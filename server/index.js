3e649051cc2d9f4e5cd5828a83bfe46ca67ab9b2
Matt Sadauckas <matthewsadauckas@gmail.com>
2017-12-04 09:35:00 -0700

    WIP Build app framework

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const Rx = require('rxjs/Rx')
 
 const app = express()
+const dotenv = require('dotenv').config()
+const eiaKey = process.env.EIA_API_KEY
 
 app.set('port', process.env.PORT || 3001)
 
@@ -11,9 +13,16 @@ if (process.env.NODE_ENV === 'production') {
 
 app.get('/api', (req, res) => {
   //TODO: This is where the database goes
-  res.json({stuff: 'things'})
+  res.json({
+    stuff: 'things',
+    cheese: 'puffs',
+    key: eiaKey,
+  })
 })
 
-app.listen(app.get('port'), () => {
-  console.log(`Find the server at: http://localhost:${app.get('port')}`)
+app.listen(app.get('port'), err => {
+  if (err) {
+    console.error('An error occured:', err)
+  }
+  console.log(`Find the server at: http://localhost:${app.get('port')}, api key is ${eiaKey}`)
 })
