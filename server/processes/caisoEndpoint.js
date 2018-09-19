7ad34b96d339d7c9798d13a7461d8d461123c563
Matt Sadauckas <matthewsadauckas@gmail.com>
2018-09-18 21:22:41 -0600

    feat(core) Get CAISO data from url

--- server/processes/caisoEndpoint.js
+++ server/processes/caisoEndpoint.js
@@ -10,55 +10,35 @@ const { checkStatus } = require('../utils/')
 
 const caisoEndpoint = () => new Promise( (resolve, reject) => {
 
-  const dir = 'server/processes/downloads/'
-
   const url = `http://oasis.caiso.com/oasisapi/SingleZip?queryname=SLD_REN_FCST&market_run_id=DAM&startdatetime=20180819T07:00-0000&enddatetime=20180820T07:00-0000&version=1`
+
   const stream = request(url)
 
-  // resolve(stream)
-  const fileName = 'test.zip'
+  const dir = 'server/processes/downloads/'
+
+  const fileName = 'caiso.zip'
+
   const file = fs.createWriteStream(dir + fileName)
 
   stream.on('data', data => file.write(data) )
     .on('end', () => {
       file.end()
-      const xml = fs.createReadStream(dir + fileName)
-        .pipe(unzipper.Extract({path: 'server/processes/output'}))
-      // xml.buffer()
-      // .then( buffer => buffer.toString() )
-      // .then( str => convert.xml2json(str,
-      //   {
-      //     compact: false,
-      //     spaces: 2
-      //   })
-      // )
-      // .then(resolve)
-      resolve('success:', xml)
+      fs.createReadStream(dir + fileName)
+        // .pipe(unzipper.Extract({path: 'server/processes/output'}))
+        .pipe(unzipper.Parse())
+        .on('entry', entry => {
+          entry.buffer()
+          .then( buffer => buffer.toString() )
+          .then( str => convert.xml2json(str,
+            {
+              compact: true,
+              spaces: 2
+            })
+          )
+          .then(resolve)
+        })
     })
     .on('error', reject)
-
-
-
-
-
-
-
-  // Demo stuff that works
-    // const testUrl = 'http://www2.census.gov/geo/tiger/TIGER2015/ZCTA5/tl_2015_us_zcta510.zip'
-  // unzipper.Open.url(request, testUrl)
-  // .then( res => {
-  //   const file = res.files.filter( f => f.path === 'tl_2015_us_zcta510.shp.iso.xml')[0]
-  //   return file.buffer()
-  // })
-  // .then( buffer => buffer.toString() )
-  // .then( str => convert.xml2json(str,
-  //   {
-  //     compact: false,
-  //     spaces: 2
-  //   })
-  // )
-  // .then(resolve)
-  // .catch(reject)
 })
 
 module.exports = {
