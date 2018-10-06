b6541055db576e9bdb3a00d88ea330b364998a51
Matt Sadauckas <matthewsadauckas@gmail.com>
2018-10-05 23:45:05 -0600

    feat(core) Build running average calculator

--- server/utils/dataScienceUtils.js
+++ server/utils/dataScienceUtils.js
@@ -1,30 +1,17 @@
-const calculateMovingAverage = (data, period, key) => {
-  console.log('data:', data, '\nlength:', data.length)
-  const calc = data.reduce( (arr, d, i) => data[i - 1] ?
-    [
-      ...arr,
-      {
-        timestamp: d.timestamp,
-        mvAvg: data.slice(Math.max(0, i - period), i + 1)
-                .reduce( ([sum, n], obj, i, arr) =>
-                {
-                  console.log(
-                    'i:', i,
-                    '\nsum:', sum,
-                    '\nn:', n,
-                    '\nlength:', arr.length
-                  );
-                  return i !== (arr.length - 1) ?
-                  [sum + obj[key], n + 1] :
-                  (sum + obj[key])/(n + 1)
-                }
-                , [0, 0]),
-      }
-    ] :
-    arr, [])
-    console.log('calc:', calc);
-  return calc
-}
+const calculateMovingAverage = (data, period, key) => data.reduce( (arr, d, i) => data[i - 1] ?
+  [
+    ...arr,
+    {
+      timestamp: d.timestamp,
+      mvAvg: data
+              .slice(Math.max(0, i - (period - 1)), i + 1)
+              .reduce( ([sum, n], obj, i, arr) => i !== (arr.length - 1) ?
+                [sum + obj[key], n + 1] :
+                (sum + obj[key])/(n + 1)
+              , [0, 0]),
+    }
+  ] :
+  arr, [])
 
 module.exports = {
   calculateMovingAverage,
