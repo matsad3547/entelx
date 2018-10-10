f4ce89b4a2123c70fbc1570013497b846a0565cd
Matt Sadauckas <matthewsadauckas@gmail.com>
2018-10-10 10:10:06 -0600

    feat(core) Add price data aggregation

--- server/utils/dataScienceUtils.js
+++ server/utils/dataScienceUtils.js
@@ -8,7 +8,8 @@ const calculateMovingAverage = (data, key, period) => data.reduce( (arr, d, i) =
       [key]: d[key],
       mvgAvg: data
               .slice(Math.max(0, i - (period - 1)), i + 1)
-              .reduce( ([sum, n], obj, i, arr) => i !== (arr.length - 1) ?
+              .reduce( ([sum, n], obj, i, arr) =>
+              i !== (arr.length - 1) ?
                 [sum + obj[key], n + 1] :
                 (sum + obj[key])/(n + 1)
               , [0, 0]),
@@ -24,6 +25,59 @@ const calculateScore = (data, key) => data.map( d => ({
   })
 )
 
+const calculateArbitrage = (data, key, period, threshold) => data.reduce( (obj, d, i) =>
+  i < data.length - 1 ?
+  {
+    data: obj.data,
+    chargeVol: {
+      prcSum: data[i].score < -threshold ?
+        obj.chargeVol.prcSum + data[i][key] :
+        obj.chargeVol.prcSum,
+      n: data[i].score < -threshold ?
+        obj.chargeVol.n + 1 :
+        obj.chargeVol.n,
+    },
+    dischargeVol: {
+      prcSum: data[i].score > threshold ?
+        obj.dischargeVol.prcSum + data[i][key] :
+        obj.dischargeVol.prcSum,
+      n: data[i].score > threshold ?
+        obj.dischargeVol.n + 1 :
+        obj.dischargeVol.n,
+    },
+  } :
+  {
+    data: obj.data,
+    chargeVol: {
+      avgPrc: data[i].score < -threshold ?
+      (obj.chargeVol.prcSum + data[i][key]) / (obj.chargeVol.n + 1) :
+      obj.chargeVol.prcSum / obj.chargeVol.n,
+      n: data[i].score < -threshold ?
+      obj.chargeVol.n + 1 :
+      obj.chargeVol.n,
+    },
+    dischargeVol: {
+      avgPrc: data[i].score > threshold ?
+      (obj.dischargeVol.prcSum + data[i][key]) / (obj.dischargeVol.n + 1) :
+      obj.dischargeVol.prcSum / obj.dischargeVol.n,
+      n: data[i].score > threshold ?
+      obj.dischargeVol.n + 1 :
+      obj.dischargeVol.n,
+    },
+  },
+  {
+    data,
+    chargeVol: {
+      prcSum: 0,
+      n: 0,
+    },
+    dischargeVol: {
+      prcSum: 0,
+      n: 0,
+    },
+  }
+)
+
 const scoreValues = pipeData(
   calculateMovingAverage,
   calculateScore,
@@ -31,5 +85,8 @@ const scoreValues = pipeData(
 
 module.exports = {
   calculateMovingAverage,
+  calculateScore,
+  calculateArbitrage,
+  pipeData,
   scoreValues,
 }
--- server/utils/test/dataScienceUtils.test.js
+++ server/utils/test/dataScienceUtils.test.js
@@ -2,8 +2,38 @@ const { timeSeriesData } = require('./mocks/timeSeries')
 
 const {
   calculateMovingAverage,
-  scoreValues,
+  calculateScore,
+  calculateArbitrage,
+  pipeData,
 } = require('../dataScienceUtils')
+
+const testData = [
+  //centered on 3
+  {
+    timestamp: 1,
+    val: 3,
+  },
+  {
+    timestamp: 2,
+    val: 7,
+  },
+  {
+    timestamp: 3,
+    val: -1,
+  },
+  {
+    timestamp: 4,
+    val: 6,
+  },
+  {
+    timestamp: 5,
+    val: 0,
+  },
+  {
+    timestamp: 6,
+    val: 8,
+  },
+]
 
 describe('calculateMovingAverage', () => {
 
@@ -59,60 +89,35 @@ describe('calculateMovingAverage', () => {
   })
 })
 
-describe('scoreValues', () => {
+describe('calculateScore', () => {
 
-  const testData = [
-    //centered on 3
-    {
-      timestamp: 1,
-      val: 3,
-    },
-    {
-      timestamp: 2,
-      val: 7,
-    },
-    {
-      timestamp: 3,
-      val: -1,
-    },
-    {
-      timestamp: 4,
-      val: 6,
-    },
-    {
-      timestamp: 5,
-      val: 0,
-    },
-    {
-      timestamp: 6,
-      val: 8,
-    },
-  ]
+  let period = 5
+  let key = 'val'
+  const getAvgData = pipeData(
+    calculateMovingAverage,
+  )
+
+  let data
+
+  beforeEach( () => {
+      data = getAvgData(testData, key, period)
+  })
 
   test('should return an array', () => {
-    const data = testData
-    const period = 5
-    const key = 'val'
     const expected = true
-    const actual = Array.isArray(scoreValues(data, key, period))
+    const actual = Array.isArray(calculateScore(data, key))
     expect(actual).toEqual(expected)
   })
 
   test('should return an array 5 items long', () => {
-    const data = testData
-    const period = 5
-    const key = 'val'
     const expected = 5
-    const actual = scoreValues(data, key, period).length
+    const actual = calculateScore(data, key).length
     expect(actual).toEqual(expected)
   })
 
   test('should return an object with `timestamp`, `mvAvg`, and `score` properties and a property for whichever key was passed in', () => {
-    const data = testData
-    const period = 5
-    const key = 'val'
     const expected = new Array(5).fill(true)
-    const arr = scoreValues(data, key, period)
+    const arr = calculateScore(data, key)
     const keyArr = arr.map( obj => Object.keys(obj) )
     const actual = keyArr.map( k =>
       k.includes('timestamp') &&
@@ -124,58 +129,145 @@ describe('scoreValues', () => {
   })
 
   test('should have a `mvgAvg` value of `4` for its last entry for the last 5 entries', () => {
-    const data = testData
-    const period = 5
-    const key = 'val'
     const expected = 4
-    const actual = scoreValues(data, key, period)[4].mvgAvg
+    const actual = calculateScore(data, key)[4].mvgAvg
     expect(actual).toEqual(expected)
   })
 
   test('should have a `score` value of `2` for its last entry', () => {
-    const data = testData
-    const period = 5
-    const key = 'val'
     const expected = 1
-    const actual = scoreValues(data, key, period)[4].score
+    const actual = calculateScore(data, key)[4].score
     expect(actual).toEqual(expected)
   })
 
   test('should have a `score` value of `0.4` for its first entry', () => {
-    const data = testData
-    const period = 5
-    const key = 'val'
     const expected = .4
-    const actual = scoreValues(data, key, period)[0].score
+    const actual = calculateScore(data, key)[0].score
     expect(actual).toEqual(expected)
   })
 
   test('should have a `score` value of `-1` for its second to last entry', () => {
-    const data = testData
-    const period = 5
-    const key = 'val'
     const expected = -1
-    const actual = scoreValues(data, key, period)[3].score
+    const actual = calculateScore(data, key)[3].score
     expect(actual).toEqual(expected)
   })
-  
-  //visualization purposes only
-  // test('should look like an array of objects', () => {
-  //   const data = testData
-  //   const period = 5
-  //   const key = 'val'
-  //   const expected = []
-  //   const actual = scoreValues(data, key, period)
-  //   expect(actual).toEqual(expected)
-  // })
+
+  // visualization purposes only
+  test('should look like an array of objects', () => {
+    const expected = [
+      {
+        mvgAvg: 5,
+        score: 0.4,
+        timestamp: 2,
+        val: 7,
+      },
+      {
+        mvgAvg: 3,
+        score: -1.3333333333333333,
+        timestamp: 3,
+        val: -1,
+      },
+      {
+        mvgAvg: 3.75,
+        score: 0.6,
+        timestamp: 4,
+        val: 6,
+      },
+      {
+        mvgAvg: 3,
+        score: -1,
+        timestamp: 5,
+        val: 0,
+      },
+      {
+        mvgAvg: 4,
+        score: 1,
+        timestamp: 6,
+        val: 8,
+      },
+    ]
+    const actual = calculateScore(data, key)
+    expect(actual).toEqual(expected)
+  })
 
   // //visualization purposes only
   // test('should look like an array of objects', () => {
-  //   const data = timeSeriesData
   //   const period = 10
   //   const key = 'lmp'
+  //   const data = getAvgData(timeSeriesData, key, period)
   //   const expected = []
-  //   const actual = scoreValues(data, key, period)
+  //   const actual = calculateScore(data, key)
   //   expect(actual).toEqual(expected)
   // })
+})
+
+describe('calculateArbitrage', () => {
+
+  let period = 5
+  let key = 'val'
+  const getScoreData = pipeData(
+    calculateMovingAverage,
+    calculateScore,
+  )
+
+  let data
+
+  beforeEach( () => {
+    data = getScoreData(testData, key, period)
+  })
+
+  test('should return the passed in data', () => {
+    const actual = calculateArbitrage(data, key, period, .5).data
+    const expected = [
+      {
+        mvgAvg: 5,
+        score: 0.4,
+        timestamp: 2,
+        val: 7,
+      },
+      {
+        mvgAvg: 3,
+        score: -1.3333333333333333,
+        timestamp: 3,
+        val: -1,
+      },
+      {
+        mvgAvg: 3.75,
+        score: 0.6,
+        timestamp: 4,
+        val: 6,
+      },
+      {
+        mvgAvg: 3,
+        score: -1,
+        timestamp: 5,
+        val: 0,
+      },
+      {
+        mvgAvg: 4,
+        score: 1,
+        timestamp: 6,
+        val: 8,
+      },
+    ]
+    expect(actual).toEqual(expected)
+  })
+
+  test('should return an object with a correct `chargeVol` object', () => {
+    const actual = calculateArbitrage(data, key, period, .5).chargeVol
+    const expected = {
+      avgPrc: -.5,
+      n: 2,
+    }
+    expect(actual).toEqual(expected)
+  })
+
+  test('should return an object with a correct `dischargeVol` object', () => {
+    const actual = calculateArbitrage(data, key, period, .5).dischargeVol
+    const expected = {
+      avgPrc: 7,
+      n: 2,
+    }
+    expect(actual).toEqual(expected)
+  })
 })
