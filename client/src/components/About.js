8211c613194fb2293358ba9023fad0a8b0a72816
Matt Sadauckas <matthewsadauckas@gmail.com>
2018-10-22 23:07:42 -0600

    feat(core) Add text to about

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -4,7 +4,14 @@ const About = () => (
 
   <div>
     <h1>About</h1>
+    <p style={styles.text}>This project is the work of Matt Sadauckas.  Motivated, knowledgeable collaborators and critical feedback sought!</p>
   </div>
 )
+
+const styles = {
+  text: {
+    padding: '1em',
+  }
+}
 
 export default About
