60e79e6be0c33f18695cab90db12ed7773be2762
Matt Sadauckas <matthewsadauckas@gmail.com>
2018-10-30 21:34:48 -0600

    feat(core) Add id to node table

--- server/knexfile.js
+++ server/knexfile.js
@@ -8,7 +8,9 @@ module.exports = {
   client: 'mysql',
   connection: {
     user: 'root',
-    password: NODE_ENV === 'production' ? PROD_DB_PASSWORD : DEV_DB_PASSWORD,
+    // TODO Password is not brought from env when running migrations
+    password: 'password',
+    // password: NODE_ENV === 'production' ? PROD_DB_PASSWORD : DEV_DB_PASSWORD,
     database: 'energy_storage_marketplace',
   }
 }
