e2099956bf37f546c1600d909c738a7598ed8cef
Matt Sadauckas <matthewsadauckas@gmail.com>
2018-10-30 20:45:07 -0600

    feat(core) Add map to project creation

--- client/src/containers/ProjectCreation.js
+++ client/src/containers/ProjectCreation.js
@@ -1,21 +1,39 @@
 import React from 'react'
 
+import Map from './map/Map'
 import { colors } from '../config/styles'
 
-const ProjectCreation = () => (
+const ProjectCreation = () => {
 
-  <div style={styles.root}>
-    project creation form goes here
-    * Enter an address or pick a location on the map
-    * Project name
-    * Project capacity (MW) (MWh)
-    
-  </div>
-)
+  const lat = 37.5
+  const lng = -117.5
+
+  return (
+
+    <div style={styles.root}>
+      <div style={styles.form}>
+        * Project name
+        * Project capacity (MW) (MWh)
+        project creation form goes here
+        * Enter an address or pick a location on the map
+      </div>
+      <Map
+        center={[lng, lat]}
+        zoom={5}
+        />
+    </div>
+  )
+}
 
 const styles = {
   root: {
+    display: 'flex',
+    justifyContent: 'space-around',
     color: colors.text,
   },
+  form: {
+    width: '45%'
+  },
+
 }
 export default ProjectCreation
