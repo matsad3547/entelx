ec18a610505a64a473b13786df5dec0cf0013292
Matt Sadauckas <matthewsadauckas@gmail.com>
2018-11-01 22:20:51 -0600

    feat(core) Add routing for project roi and dashboard

---
+++ client/src/containers/Project.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import Header3 from '../components/Header3'
+import Header4 from '../components/Header4'
+import DisableableLink from '../components/DisableableLink'
+
+
+const Project = ({match}) => {
+
+  const {
+    url,
+    params,
+  } = match
+
+  const cleanUrl = url.replace('/project/' + params.projectId, '')
+
+  return (
+    <div>
+      <Header3 content="Project Home"/>
+      <p>Project {match.params.projectId}</p>
+      <div style={styles.root}>
+        <nav style={styles.nav}>
+          <DisableableLink to={`${cleanUrl}/roi/${params.projectId}`}>
+            <Header4 content="Project ROI" />
+          </DisableableLink>
+          <DisableableLink to={`${cleanUrl}/dashboard/${params.projectId}`}>
+            <Header4 content="Project Dashboard" />
+          </DisableableLink>
+        </nav>
+      </div>
+    </div>
+  )
+}
+
+const styles = {
+  nav: {
+    display: 'inline-flex',
+    width: '18em',
+    justifyContent: 'space-between',
+    padding: '1em 3em',
+  },
+}
+
+Project.propTypes = {
+  content: PropTypes.string,
+}
+
+export default Project
--- client/src/containers/ProjectDashboard.js
+++ client/src/containers/ProjectDashboard.js
@@ -2,10 +2,11 @@ import React from 'react'
 
 import { colors } from '../config/styles'
 
-const ProjectDashboard = () => (
+const ProjectDashboard = ({match}) => (
 
   <div style={styles.root}>
     <h1>Project Dashboard</h1>
+    <p>Project id: {match.params.projectId}</p>
   </div>
 )
 
