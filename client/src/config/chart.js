5027c8c9bce3cd697dd5310f2ca520e875c410fa
Matt Sadauckas <matthewsadauckas@gmail.com>
2018-11-03 10:36:18 -0600

    feat(core) Adjust styling

---
+++ client/src/config/chart.js
@@ -0,0 +1,57 @@
+export const trendColors = [
+  '#03CEA4',
+  '#78C3FB',
+  '#89A6FB',
+  '#7067CF',
+  '#345995',
+  '#A11692',
+  '#861388',
+]
+
+export const lineDataFormat = {
+  lmp: {
+    unit: '$/Mwh',
+    label: 'Locational Marginal Price',
+    color: trendColors[0],
+    format: value => Math.round((value * 100))/100,
+  },
+  temperature: {
+    unit: '°C',
+    label: 'Temperature',
+    color: trendColors[1],
+    format: value => Math.round(value),
+  },
+  windSpeed: {
+    unit: ' m/s',
+    label: 'Wind Speed',
+    color: trendColors[2],
+    format: value => Math.round(value),
+  },
+  windBearing: {
+    unit: '°',
+    label: 'Wind Bearing',
+    color: trendColors[3],
+    format: value => value ? value : 'n/a',
+  },
+  cloudCover: {
+    unit: '%',
+    label: 'Cloud Cover',
+    color: trendColors[4],
+    format: value => value * 100,
+  },
+  mvgAvg: {
+    unit: '$/Mwh',
+    label: 'Average Price',
+    color: trendColors[6],
+    format: value => Math.round((value * 100)) / 100,
+  },
+}
+
+export const barDataFormat = {
+  score: {
+    unit: '',
+    label: 'Score',
+    color: value => value > 0 ? 'red' : 'green',
+    format: value => Math.round((value * 1000)) / 1000,
+  }
+}
