cff167313d0cafb5ea466bbf59ee7d1f09026629
Matt Sadauckas <matthewsadauckas@gmail.com>
2018-11-04 22:27:46 -0700

    feat(core) Change power and energy columns to decimal

---
+++ server/migrations/20181104221923_change_project_column_types.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.table('project', t => {
+    t.dropColumn('power')
+    t.dropColumn('energy')
+  })
+  .then( () => knex.schema.table('project', t => {
+      t.decimal('power').notNullable
+      t.decimal('energy').notNullable
+    })
+  )
+};
+
+exports.down = function(knex, Promise) {
+
+};
