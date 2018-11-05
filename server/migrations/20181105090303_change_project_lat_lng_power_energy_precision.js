d662d8040d057e04630edc19adac34cb1850fcb6
Matt Sadauckas <matthewsadauckas@gmail.com>
2018-11-05 09:16:21 -0700

    feat(core) Change column precision

---
+++ server/migrations/20181105090303_change_project_lat_lng_power_energy_precision.js
@@ -0,0 +1,20 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.table('project', t => {
+    t.dropColumn('power')
+    t.dropColumn('energy')
+    t.dropColumn('lat')
+    t.dropColumn('lng')
+  })
+  .then( () => knex.schema.table('project', t => {
+      t.decimal('power', 6, 3).notNullable
+      t.decimal('energy', 6, 3).notNullable
+      t.decimal('lat', 9, 5).notNullable
+      t.decimal('lng', 9, 5).notNullable
+    })
+  )
+};
+
+exports.down = function(knex, Promise) {
+
+};
