3f4251c0f4a2c91a3528fea6f02d786a44529426
Matt Sadauckas <matthewsadauckas@gmail.com>
2018-11-07 09:20:49 -0700

    feat(core) Find closest by id

--- server/utils/index.js
+++ server/utils/index.js
@@ -9,10 +9,16 @@ const { writeToFile } = require('./fileUtils')
 
 const { scoreValues } = require('./dataScienceUtils')
 
+const { findClosestId } = require('./geoUtils')
+
+const { findByLatLng } = require('./dbUtils')
+
 module.exports = {
   millisToSeconds,
   tsToMillis,
   checkStatus,
   writeToFile,
   scoreValues,
+  findClosestId,
+  findByLatLng,
 }
--- server/utils/test/geoUtils.test.js
+++ server/utils/test/geoUtils.test.js
@@ -1,7 +1,179 @@
 const {
   haversineDist,
   degToRad,
+  findClosestId,
 } = require('../geoUtils')
+
+const selectedNodes = [
+  {
+    name: 'DESRT PK_GNODEIT',
+    type: 'GEN',
+    control_area: 'NV',
+    lat: 39.75,
+    lng: -118.95,
+    start_date: null,
+    end_date: null,
+    max_mw: null,
+    id: 1473,
+    score: 0
+  },
+  {
+    name: 'EAGLE_LNODE01',
+    type: 'LOAD',
+    control_area: 'NV',
+    lat: 39.62,
+    lng: -119.21,
+    start_date: null,
+    end_date: null,
+    max_mw: null,
+    id: 1702,
+    score: 0
+  },
+  {
+    name: 'EAGLE_LNODE02',
+    type: 'LOAD',
+    control_area: 'NV',
+    lat: 39.62,
+    lng: -119.21,
+    start_date: null,
+    end_date: null,
+    max_mw: null,
+    id: 1703,
+    score: 0
+  },
+  {
+    name: 'EAGLE_LNODE18',
+    type: 'LOAD',
+    control_area: 'NV',
+    lat: 39.62,
+    lng: -119.21,
+    start_date: null,
+    end_date: null,
+    max_mw: null,
+    id: 1704,
+    score: 0
+  },
+  {
+    name: 'NDL-BUSH_LNODE-1',
+    type: 'LOAD',
+    control_area: 'NV',
+    lat: 39.62,
+    lng: -119.21,
+    start_date: null,
+    end_date: null,
+    max_mw: null,
+    id: 3936,
+    score: 0
+  },
+  {
+    name: 'NDL-EGL_LNODE-1',
+    type: 'LOAD',
+    control_area: 'NV',
+    lat: 39.62,
+    lng: -119.21,
+    start_date: null,
+    end_date: null,
+    max_mw: null,
+    id: 3937,
+    score: 0
+  },
+  {
+    name: 'NDL-EGL_LNODE-2',
+    type: 'LOAD',
+    control_area: 'NV',
+    lat: 39.62,
+    lng: -119.21,
+    start_date: null,
+    end_date: null,
+    max_mw: null,
+    id: 3938,
+    score: 0
+  },
+  {
+    name: 'NGHTGALE_GNODEIT',
+    type: 'GEN',
+    control_area: 'NV',
+    lat: 39.8,
+    lng: -119.01,
+    start_date: null,
+    end_date: null,
+    max_mw: null,
+    id: 3973,
+    score: 0
+  },
+  {
+    name: 'NGHTGALE_LNODE18',
+    type: 'LOAD',
+    control_area: 'NV',
+    lat: 39.8,
+    lng: -119.01,
+    start_date: null,
+    end_date: null,
+    max_mw: null,
+    id: 3975,
+    score: 0
+  },
+  {
+    name: 'NVCEMNT_LNODEC1',
+    type: 'LOAD',
+    control_area: 'NV',
+    lat: 39.62,
+    lng: -119.26,
+    start_date: null,
+    end_date: null,
+    max_mw: null,
+    id: 4053,
+    score: 0
+  },
+  {
+    name: 'NVCEMNT_LNODEC2',
+    type: 'LOAD',
+    control_area: 'NV',
+    lat: 39.62,
+    lng: -119.26,
+    start_date: null,
+    end_date: null,
+    max_mw: null,
+    id: 4055,
+    score: 0
+  },
+  {
+    name: 'PATUA_GNODEIT',
+    type: 'GEN',
+    control_area: 'NV',
+    lat: 39.62,
+    lng: -119.21,
+    start_date: null,
+    end_date: null,
+    max_mw: null,
+    id: 4301,
+    score: 0
+  },
+  {
+    name: 'US GEO_GNODET',
+    type: 'GEN',
+    control_area: 'NV',
+    lat: 40.35,
+    lng: -119.34,
+    start_date: null,
+    end_date: null,
+    max_mw: null,
+    id: 5953,
+    score: 0
+  },
+  {
+    name: 'WADSWRTH_LNODETH',
+    type: 'LOAD',
+    control_area: 'NV',
+    lat: 39.65,
+    lng: -119.33,
+    start_date: null,
+    end_date: null,
+    max_mw: null,
+    id: 6078,
+    score: 0
+  },
+]
 
 describe('haversineDist', () => {
 
@@ -42,4 +214,14 @@ test('degToRad should return correct values', () => {
   const expected = Math.PI / 2
   const actual = degToRad(deg)
   expect(actual).toEqual(expected)
+})
+
+describe('findClosestId', () => {
+  test('should return a number', () => {
+    const lat = 40.0
+    const lng = -119.0
+    const actual = findClosestId(lat, lng, selectedNodes)
+    const expected = 3973
+    expect(actual).toEqual(expected)
+  })
 })
