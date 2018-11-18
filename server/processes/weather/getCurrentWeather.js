bfbedba61dff605858ba348904aa0d0134eaa46b
Matt Sadauckas <matthewsadauckas@gmail.com>
2018-11-18 15:41:14 -0700

    feat(core) Clean up oasis endpoint

--- server/processes/weather/getCurrentWeather.js
+++ server/processes/weather/getCurrentWeather.js
@@ -1,17 +1,15 @@
 const weatherKey = process.env.DARK_SKY_KEY
 const { checkStatus } = require('../../utils/')
 
-const getCurrentWeather = (lat, lng) => new Promise( (resolve, reject) => {
+const getCurrentWeather = (lat, lng) => {
 
   const url = `          https://api.darksky.net/forecast/${weatherKey}/${lat},${lng}`
 
-  console.log('url:', url);
-
-  fetch(url)
+  return fetch(url)
     .then(checkStatus)
     .then( res => res.json() )
-    .then(data => resolve(data.currently) )
-    .catch(reject)
-})
+    .then( data => data.currently )
+    .catch( err => console.error(`there was an error fetching current weather: ${err}`) )
+}
 
 module.exports = getCurrentWeather
