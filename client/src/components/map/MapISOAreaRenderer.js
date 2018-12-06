d7cf85e9e2fd5cca3ada2b3f11b2fda387f823bd
Matt Sadauckas <matthewsadauckas@gmail.com>
2018-12-06 00:02:18 -0700

    feat(core) Make location reader work correctly again

--- client/src/components/map/MapISOAreaRenderer.js
+++ client/src/components/map/MapISOAreaRenderer.js
@@ -1,21 +1,37 @@
+import { useEffect } from 'react'
 import PropTypes from 'prop-types'
 
 const MapISOAreaRenderer = ({ map, getLatLng }) => {
 
-  map.addSource('isos', {
-    type: "geojson",
-    data: 'https://opendata.arcgis.com/datasets/9d1099b016e5482c900d657f06f3ac80_0.geojson',
-  })
+  const setLayers = () => {
 
-  map.addLayer({
-    id: 'iso',
-    type: 'fill',
-    source: 'isos',
-    paint: {
-            'fill-color': '#088',
-            'fill-opacity': 0.3,
-            'fill-outline-color': '#000',
-        }
+    map.addSource('isos', {
+      type: "geojson",
+      data: 'https://opendata.arcgis.com/datasets/9d1099b016e5482c900d657f06f3ac80_0.geojson',
+    })
+
+    map.addLayer({
+      id: 'iso',
+      type: 'fill',
+      source: 'isos',
+      paint: {
+        'fill-color': '#088',
+        'fill-opacity': 0.3,
+        'fill-outline-color': '#000',
+      }
+    })
+  }
+
+  const cleanup = () => {
+    if (map && map.getSource('isos')) {
+      map.removeLayer('iso')
+      map.removeSource('isos')
+    }
+  }
+
+  useEffect( () => {
+    setLayers()
+    return () => cleanup()
   })
 
   return false
--- client/src/containers/map/MapNodeRenderer.js
+++ client/src/containers/map/MapNodeRenderer.js
@@ -80,54 +80,55 @@ const MapNodeRenderer = ({ map }) => {
 
   const handleError = err => console.error(`there was an error getting nodes: ${err}`)
 
-  if (nodes) {
-    map.addSource('nodes', {
-      type: "geojson",
-      data: nodes,
-      cluster: true,
-      clusterMaxZoom: 7, // Max zoom to cluster points on
-      clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
-    })
+  const setLayers = () => {
+    if (nodes) {
+      map.addSource('nodes', {
+        type: "geojson",
+        data: nodes,
+        cluster: true,
+        clusterMaxZoom: 7, // Max zoom to cluster points on
+        clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
+      })
 
-    map.addLayer({
-      id: "clusters",
-      type: "circle",
-      source: "nodes",
-      filter: ["has", "point_count"],
-      paint: {
-        "circle-color": 'rgba(1, 186, 239, .5)',
-        'circle-radius': [
-            "step",
-            ["get", "point_count"],
+      map.addLayer({
+        id: 'clusters',
+        type: 'circle',
+        source: 'nodes',
+        filter: ['has', 'point_count'],
+        paint: {
+          'circle-color': 'rgba(1, 186, 239, .5)',
+          'circle-radius': [
+            'step',
+            ['get', 'point_count'],
             20,
             100,
             30,
             750,
             40
-        ],
-        'circle-stroke-width': 1,
-        'circle-stroke-color': '#000',
-      }
-    })
-
-    map.addLayer({
-        id: "cluster-count",
-        type: "symbol",
-        source: "nodes",
-        filter: ["has", "point_count"],
-        layout: {
-            "text-field": "{point_count_abbreviated}",
-            "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
-            "text-size": 12
+          ],
+          'circle-stroke-width': 1,
+          'circle-stroke-color': '#000',
         }
-    });
+      })
 
-    map.addLayer({
-      id: "unclustered-point",
-      type: "circle",
-      source: "nodes",
-      filter: ["!", ["has", "point_count"]],
-      paint: {
+      map.addLayer({
+        id: 'cluster-count',
+        type: 'symbol',
+        source: 'nodes',
+        filter: ['has', 'point_count'],
+        layout: {
+          "text-field": "{point_count_abbreviated}",
+          "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+          "text-size": 12
+        }
+      });
+
+      map.addLayer({
+        id: 'unclustered-point',
+        type: "circle",
+        source: "nodes",
+        filter: ["!", ["has", "point_count"]],
+        paint: {
           'circle-color': [
             'match',
             ['get', 'controlArea'],
@@ -144,31 +145,50 @@ const MapNodeRenderer = ({ map }) => {
           'circle-radius': 5,
           'circle-stroke-width': 1,
           'circle-stroke-color': '#000',
-      }
-    })
+        }
+      })
 
-    map.on('click', 'clusters', e => {
-      const features = map.queryRenderedFeatures(e.point, { layers: ['clusters'] })
-      const clusterId = features[0].properties.cluster_id;
-      map.getSource('nodes').getClusterExpansionZoom(clusterId, (err, zoom) => {
-        if (err)
-            return
+      map.on('mouseenter', 'clusters', () => {
+        map.getCanvas().style.cursor = 'pointer'
+      })
 
-        map.easeTo({
-            center: features[0].geometry.coordinates,
-            zoom,
-        })
+      map.on('mouseleave', 'clusters', () => {
+        map.getCanvas().style.cursor = ''
+      })
+
+      map.on('click', 'clusters', onClusterClick)
+    }
+  }
+
+
+  const onClusterClick = e => {
+    const features = map.queryRenderedFeatures(e.point, { layers: ['clusters'] })
+    const clusterId = features[0].properties.cluster_id
+    map.getSource('nodes').getClusterExpansionZoom(clusterId, (err, zoom) => {
+      if (err)
+      return
+
+      map.easeTo({
+        center: features[0].geometry.coordinates,
+        zoom,
       })
     })
-
-    map.on('mouseenter', 'clusters', () => {
-        map.getCanvas().style.cursor = 'pointer'
-    })
-
-    map.on('mouseleave', 'clusters', () => {
-        map.getCanvas().style.cursor = ''
-    })
   }
+
+  const cleanup = () => {
+    if (map && map.getSource('nodes')) {
+      map.removeLayer('clusters')
+      map.removeLayer('cluster-count')
+      map.removeLayer('unclustered-point')
+      map.off('click', 'clusters', onClusterClick)
+      map.removeSource('nodes')
+    }
+  }
+
+  useEffect( () => {
+    setLayers()
+    return () => cleanup()
+  }, [nodes])
 
   return false
 }
