c326fba707542083ae0b8037e089fe612519c450
Matt Sadauckas <matthewsadauckas@gmail.com>
2019-01-04 07:53:46 -0700

    feat(core) Misc. changes

--- server/project/deleteProjectById.js
+++ server/project/deleteProjectById.js
@@ -9,24 +9,21 @@ const deleteProjectById = (req, res) => {
 
   return readTableRows('project', {id,})
     .then( project => {
+
       const { pid } = project[0]
-      const handleError = e => console.error('Error ending process', pid,':', e)
+
+      const handleError = e => console.error(`Error ending process ${pid}:`, e)
+
       try {
-        console.log('trying!');
         process.kill(pid)
-      } catch (e) {
+      }
+      catch (e) {
         handleError(e)
       }
       return deleteTableRows('project', {id,})
         .then( ({success}) => success ? res.sendStatus(200) : res.sendStatus(404) )
         .catch( err => console.error(`Error at deleteProjectById: ${err}`) )
     })
-
-  // TODO Get the PID from the project table, then run `process.kill(<pid>)` before deleting the project
-
-  // return deleteTableRows('project', {id,})
-  //   .then( ({success}) => success ? res.sendStatus(200) : res.sendStatus(404) )
-  //   .catch( err => console.error(`Error at deleteProjectById: ${err}`) )
 }
 
 module.exports = deleteProjectById
