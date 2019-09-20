ec43e6abe55525c697d0eaecf23b4a620e37b3d9
Matt Sadauckas <matthewsadauckas@gmail.com>
2019-09-20 07:52:45 -0600

    Build client

---
+++ server/public/precache-manifest.90e523fa3fb7258d35dec0de84353ecf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "056f896242e1ee09885160025dc3b362",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ba612ccce71094bf813",
+    "url": "/static/css/main.47730f72.chunk.css"
+  },
+  {
+    "revision": "c7e12d5a45c4e378c469",
+    "url": "/static/js/2.cd5bef24.chunk.js"
+  },
+  {
+    "revision": "4ba612ccce71094bf813",
+    "url": "/static/js/main.05d369ea.chunk.js"
+  },
+  {
+    "revision": "db6be01aac7cb1c6d4ff",
+    "url": "/static/js/runtime~main.0033641b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
