103a3e6b93697a48e3feb0577812242659a534fa
Matt Sadauckas <matthewsadauckas@gmail.com>
2019-10-26 17:43:14 -0600

    Add host

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -11,6 +11,7 @@ module.exports = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    host: process.env.DB_HOST || '',
   },
   migrations: {
     directory: './migrations',
