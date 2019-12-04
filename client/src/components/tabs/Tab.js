31ea6ea248cb99998f76f589064a6344a0e9e299
Matt Sadauckas <matthewsadauckas@gmail.com>
2019-12-03 23:56:22 -0700

    Add variables

---
+++ client/src/components/tabs/Tab.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Tab = ({
+  link,
+  label,
+}) => {
+
+  return (
+    <div>
+      Aggregate data goes here
+    </div>
+  )
+}
+
+export default Tab
---
+++ client/src/components/tabs/Tabs.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Tabs = ({
+  values,
+}) => {
+
+  return (
+    <div>
+      Aggregate data goes here
+    </div>
+  )
+}
+
+export default Tabs
