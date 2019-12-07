137aeffc42ffbe3b75b366a89399529e0f4f9a67
Matt Sadauckas <matthewsadauckas@gmail.com>
2019-12-07 09:24:38 -0500

    Extract date range selector

--- client/src/components/dateRangeControl/DateRangeControl.js
+++ client/src/components/dateRangeControl/DateRangeControl.js
@@ -1,24 +1,15 @@
-import React, {useState, useRef} from 'react'
+import React, {useRef} from 'react'
 import PropTypes from 'prop-types'
-import moment from 'moment-timezone'
 
-import DataTimeDisplay from '../DataTimeDisplay'
-import Header4 from '../Header4'
-import Button from '../button/'
-
-import TimeIncrementSelect from './TimeIncrementSelect'
+import DateRangeSelector from './DateRangeSelector'
 import DateRangeDisplay from './DateRangeDisplay'
 
-import { roundMomentToMinutes } from '../../utils/'
-
 import {
-  timeIncrements,
   colors,
   boxShadow,
 } from '../../config/'
 
 import {
-  useMinDate,
   useHandleOutsideClick,
 } from '../../hooks/'
 
@@ -41,94 +32,17 @@ const DateRangeControl = ({
 
   useHandleOutsideClick(timeSelectionRef, onOutsideClick)
 
-  const getNow = () => roundMomentToMinutes(moment(), 5)
-
-  const incrementsArr = Object.keys(timeIncrements)
-
-  const [timeIncrement, setTimeIncrement] = useState(incrementsArr[1])
-
-  const onIncrement = moment => moment.clone().add(1, timeIncrement)
-
-  const onDecrement = moment => moment.clone().subtract(1, timeIncrement)
-
-  const onIncrementStartTime = () => {
-    const incremented = onIncrement(startTime)
-
-    incremented.isBefore(endTime) ? setStartTime(incremented) : setStartTime(endTime.clone().subtract(1, 'day'))
-  }
-
-  const onDecrementStartTime = () => {
-
-    const decremented = onDecrement(startTime)
-
-    decremented.isAfter(minDate) ? setStartTime(decremented) : setStartTime(moment.tz(minDate, timeZone))
-  }
-
-  const onIncrementEndTime = () => {
-    const incremented = onIncrement(endTime)
-
-    incremented.isBefore(getNow()) ? setEndTime(incremented) : setEndTime(getNow())
-  }
-
-  const onDecrementEndTime = () => {
-    const decremented = onDecrement(endTime)
-
-    decremented.isAfter(startTime) ? setEndTime(decremented) : setEndTime(startTime.clone().add(1, 'day'))
-  }
-
-  const onTimeIncrementSelect = e => setTimeIncrement(e.target.value)
-
-  const minDate = useMinDate(projectId)
-
   return (
     <div style={styles.root}>
       {
         displayDRS &&
-        <div
-          ref={timeSelectionRef}
-          style={styles.drs} >
-          <div style={styles.header}>
-            <Header4 content="Select Date Range"/>
-          </div>
-          <DateRangeDisplay
+        <div ref={timeSelectionRef}>
+          <DateRangeSelector
+            setStartTime={setStartTime}
+            setEndTime={setEndTime}
             startTime={startTime}
             endTime={endTime}
-            timeZone={timeZone}
-            />
-          <div
-            style={styles.controls} >
-            <div style={styles.buttons}>
-              <Button
-                value={`- ${timeIncrements[timeIncrement].label}`}
-                type="primary"
-                onClick={onDecrementStartTime}
-                />
-              <Button
-                value={`+ ${timeIncrements[timeIncrement].label}`}
-                type="primary"
-                onClick={onIncrementStartTime}
-                />
-            </div>
-            <div style={styles.buttons}>
-              <Button
-                value={`- ${timeIncrements[timeIncrement].label}`}
-                type="primary"
-                onClick={onDecrementEndTime}
-                />
-              <Button
-                value={`+ ${timeIncrements[timeIncrement].label}`}
-                type="primary"
-                onClick={onIncrementEndTime}
-                />
-            </div>
-          </div>
-          <TimeIncrementSelect
-            onSelect={onTimeIncrementSelect}
-            selected={timeIncrement}
-            />
-          <DataTimeDisplay
-            message="Earliest data currently available is from"
-            isoString={minDate}
+            projectId={projectId}
             timeZone={timeZone}
             />
         </div>
