87e572871d3beaa29b92bd2404aba5641f0139bb
Matt Sadauckas <matthewsadauckas@gmail.com>
2019-12-08 22:42:39 -0500

    Remove extraneous db migrations

--- server/db/migrations/20191005170013_remove_mvg_avg_and_score_columns.js
+++ server/db/migrations/20191005170013_remove_mvg_avg_and_score_columns.js
@@ -1,9 +1,7 @@
 
 exports.up = knex => knex.schema.table('price', t => {
-  return Promise.all([
-    knex.schema.hasColumn('price', 'mvg_avg').then( exists => exists && t.dropColumn('mvg_avg')),
-    knex.schema.hasColumn('price', 'score').then( exists => exists && t.dropColumn('score')),
-  ])
+  t.dropColumn('mvg_avg')
+  t.dropColumn('score')
 })
 
 exports.down = knex => knex.schema.table('project', t => {
